9090a93d64fa34d0205b390418435a4330d7a14f
Md Ashik <mostofabilla@gmail.com>
2023-12-04 23:11:08 +0600

    ssr added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {afterRender, AfterRenderOptions, Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ssr-rnd';
+
+  constructor() {
+    afterRender(() =>  {
+      console.log(window)
+      console.log(document)
+    })
+  }
+
+  ngOnInit() {
+  }
 }
